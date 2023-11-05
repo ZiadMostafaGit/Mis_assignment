1b3efb3bb770a03529fdd296e0e597290fb22bc5
ZiadMostafaGit <mygithub2003@gmail.com>
2023-11-05 09:39:18 +0200

    update

---
+++ assingment/src/assingment/assignment10.java
@@ -0,0 +1,54 @@
+package assingment;
+import java.util.Scanner;
+/*
+Ziad Mostafa Elsaid
+
+*/
+
+public class assignment10 {
+
+public static void main1(String[] args)
+{
+    
+    
+    
+    int i, j, rows;
+    
+    Scanner input = new Scanner( System.in);
+    System.out.print( "Enter the number of rows: "); 
+
+    rows = input.nextInt();
+
+    
+//   for ( i = rows; i >= 0; i--) {
+//            for (j = 0; j < rows - i; j++) {
+//                System.out.print(" ");
+//            }
+//
+//            for (j = 0; j <= i * 2; j++) {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println();
+//        }
+
+
+//    for ( i = 0; i <rows ; i++) {
+//            for (j = 0; j < rows - i; j++) {
+//                System.out.print(" ");
+//            }
+//
+//            for (j = 0; j <= i * 2; j++) {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println();
+//        }
+
+
+
+
+}
+
+
+}
---
+++ assingment/src/assingment/assignment6.java
@@ -0,0 +1,40 @@
+package assingment;
+import java.util.Scanner;
+/**
+ *
+ * @author ziad
+ */
+public class assignment6 {
+    
+
+    public static void main(String[] args) {
+        
+        Scanner input=new Scanner(System.in);
+        double num1,num2;
+        
+        System.out.println("Enter num1 ");
+        num1=input.nextDouble();
+        
+        System.out.println("Enter num2");
+        num2=input.nextDouble();
+        
+        
+        System.out.println(
+                "Num1+num2 = " + (num1+num2)+
+                "\nNum1-Num2 = "+(num1-num2)+
+                "\nNum1*num2 = "+(num1*num2)+
+                "\nNum1/Num2 = "+(num1/num2));
+        
+
+           
+        
+        
+        
+        
+        
+    }
+
+
+
+
+}
