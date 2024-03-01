149b215c1dbe1b5bd155fefee057b5004b3be40e
ZiadMostafaGit <mygithub2003@gmail.com>
2024-03-01 15:56:28 +0200

    update

--- assingment/assignment6.java
+++ assingment/assignment6.java
@@ -1,39 +1,39 @@
-import java.util.Scanner;
-/**
- *
- * @author ziad
- */
-public class assignment6 {
+// import java.util.Scanner;
+// /**
+//  *
+//  * @author ziad
+//  */
+// public class assignment6 {
     
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        Scanner input=new Scanner(System.in);
-        double num1,num2;
+//         Scanner input=new Scanner(System.in);
+//         double num1,num2;
         
-        System.out.println("Enter num1 ");
-        num1=input.nextDouble();
+//         System.out.println("Enter num1 ");
+//         num1=input.nextDouble();
         
-        System.out.println("Enter num2");
-        num2=input.nextDouble();
+//         System.out.println("Enter num2");
+//         num2=input.nextDouble();
         
         
-        System.out.println(
-                "Num1+num2 = " + (num1+num2)+
-                "\nNum1-Num2 = "+(num1-num2)+
-                "\nNum1*num2 = "+(num1*num2)+
-                "\nNum1/Num2 = "+(num1/num2));
+//         System.out.println(
+//                 "Num1+num2 = " + (num1+num2)+
+//                 "\nNum1-Num2 = "+(num1-num2)+
+//                 "\nNum1*num2 = "+(num1*num2)+
+//                 "\nNum1/Num2 = "+(num1/num2));
         
 
-           input.close();
+//            input.close();
         
         
         
         
         
-    }
+//     }
 
 
 
 
-}
+// }
